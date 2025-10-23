e79a0ddff9f3eee4a772f5a71028a17e3ed09399
BlakeT <BlakeTswift@gmail.com>
2025-10-23 13:18:24 -0600

    Initial Commit

---
+++ diskGolfProject/diskGolfProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  diskGolfProjectApp.swift
+//  diskGolfProject
+//
+//  Created by Trytten, Blake - Student on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct diskGolfProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ diskGolfProjectTests/diskGolfProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  diskGolfProjectTests.swift
+//  diskGolfProjectTests
+//
+//  Created by Trytten, Blake - Student on 10/23/25.
+//
+
+import Testing
+@testable import diskGolfProject
+
+struct diskGolfProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
